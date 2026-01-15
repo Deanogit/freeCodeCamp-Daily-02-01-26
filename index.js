861498a427016903b121544ee5441dadc2438753
Dean Howe <hideanhowe@gmail.com>
2026-01-15 11:12:56 +0000

    solution

--- index.js
+++ index.js
@@ -2,3 +2,24 @@
 // Given an integer n, return the nth number in the fibonacci sequence.
 
 // The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones. The first 10 numbers in the sequence are 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
+
+function nthFibonacci(n) {
+  console.log(n);
+
+  // define the initial sequence
+  const fib = [0, 1, 1];
+
+  // for n, calculate arr.length - 2 + arr.length -1
+
+  console.log(fib[fib.length - 2]);
+
+  for (let i = 3; i < n; i++) {
+    fib.push(fib[fib.length - 2] + fib[fib.length - 1]);
+  }
+
+  console.log(fib[fib.length - 1]);
+
+  return fib[fib.length - 1];
+
+  //return n;
+}
