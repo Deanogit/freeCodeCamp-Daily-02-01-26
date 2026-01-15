174ce3d56b26e3e1df95561c67b99d002b37fa74
Dean Howe <hideanhowe@gmail.com>
2026-01-15 14:23:41 +0000

    clean solution

--- index.js
+++ index.js
@@ -3,23 +3,37 @@
 
 // The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones. The first 10 numbers in the sequence are 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
 
+// function nthFibonacci(n) {
+//   console.log(n);
+
+//   // define the initial sequence
+//   const fib = [0, 1, 1];
+
+//   // for n, calculate arr.length - 2 + arr.length -1
+
+//   console.log(fib[fib.length - 2]);
+
+//   for (let i = 3; i < n; i++) {
+//     fib.push(fib[fib.length - 2] + fib[fib.length - 1]);
+//   }
+
+//   console.log(fib[fib.length - 1]);
+
+//   return fib[fib.length - 1];
+
+//   //return n;
+// }
+
 function nthFibonacci(n) {
-  console.log(n);
+  if (n === 1) return 0;
+  if (n === 2) return 1;
 
-  // define the initial sequence
-  const fib = [0, 1, 1];
+  const fib = [0, 1];
 
-  // for n, calculate arr.length - 2 + arr.length -1
-
-  console.log(fib[fib.length - 2]);
-
-  for (let i = 3; i < n; i++) {
-    fib.push(fib[fib.length - 2] + fib[fib.length - 1]);
+  for (let i = 2; i < n; i++) {
+    const nextNum = fib[i - 1] + fib[i - 2];
+    fib.push(nextNum);
   }
 
-  console.log(fib[fib.length - 1]);
-
-  return fib[fib.length - 1];
-
-  //return n;
+  return fib[n - 1];
 }
